24da847f3ba7822c78afb60a7a5f985705b3ba0a
jitbreeze <46838226+jitbreeze@users.noreply.github.com>
2019-12-14 14:24:58 +0900

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,49 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// OpenGL.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_OpenGLTYPE                  130
+#define ID_LIGHT_DIRECTIONAL            32771
+#define ID_LIGHT_POSITIONAL             32772
+#define ID_LIGHT_SPOTLIGHT              32773
+#define ID_LIGHT_DIRECTIONAL32774       32774
+#define ID_LIGHT_POSITIONAL32775        32775
+#define ID_LIGHT_SPOTLIGHT32776         32776
+#define ID_MO                           32777
+#define D                               32778
+#define ID_MODEL_CUBE                   32779
+#define ID_CUBE_ONECUBE                 32780
+#define ID_32781                        32781
+#define ID_32782                        32782
+#define ID_32783                        32783
+#define ID_ROTFAST                      32784
+#define ID_ROTDEF                       32785
+#define ID_ROTSLOW                      32786
+#define ID_Rotslow                      32787
+#define ID_Rotdef                       32788
+#define ID_Rotfast                      32789
+#define ID_Rotfslow                     32790
+#define ID_32791                        32791
+#define ID_32792                        32792
+#define ID_32793                        32793
+#define ID_ROTFAST                      32794
+#define ID_ROTDEF                       32795
+#define ID_ROTSLOW                      32796
+#define ID_Background                   32797
+#define ID_OnDirectional                32798
+#define ID_LIGHT_LIGHTON                32799
+#define ID_LIGHT_LIGHTOFF               32800
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32801
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
